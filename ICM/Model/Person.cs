bd24655e68f5f8806ad2ae5f2d41b73ea1208328
baptistewicht@gmail.com <wichtounet>
2011-04-19 11:16:17 +0200

    Person model

---
+++ ICM/Model/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICM.Model
+{
+    public class Person
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string FirstName {
+            get; 
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Phone
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public Department Department
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + Name;
+        }
+    }
+}
