0f26d141cf71b67707be0d25628b03dafa08e282
rukkal <rukkal@gmail.com>
2011-05-09 11:22:31 +0200

    Added comments. Implemented journaling.

--- ICM/Dao/TypesDAO.cs
+++ ICM/Dao/TypesDAO.cs
@@ -2,14 +2,27 @@
 using ICM.Utils;
 using System.Data;
 using System.Collections.Generic;
+using NLog;
 
 namespace ICM.Dao
 {
+    /// <summary>
+    /// Gives access to the types stored in the database.
+    /// </summary>
+    /// <remarks>Kean Mariotti</remarks>
     public class TypesDAO
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Creates and fills a list containing all the types stored in the database.
+        /// </summary>
+        /// <returns>The list of types.</returns>
         public List<TypeContract> GetAllTypes()
         {
             var types = new List<TypeContract>();
+
+            Logger.Debug("Retrieving all the types.");
 
             using (var reader = DBUtils.ExecuteQuery("SELECT name FROM [typeContract]", IsolationLevel.ReadUncommitted))
             {
@@ -19,9 +32,16 @@ namespace ICM.Dao
                 }
             }
 
+            Logger.Debug("Retrieved all the types.");
+
             return types;
         }
 
+        /// <summary>
+        /// Utility function used to initialize a type with its data stored in database.
+        /// </summary>
+        /// <param name="reader">The SqlResult object used to read data from the database.</param>
+        /// <returns>The initialized TypeContract object.</returns>
         private TypeContract BindType(SqlResult reader)
         {
             TypeContract type = new TypeContract();
