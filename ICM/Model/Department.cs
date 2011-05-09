41cd4ea6306336bd62966c39c2a6f052c97a4bf7
rukkal <rukkal@gmail.com>
2011-05-09 09:46:29 +0200

    Added comments.

--- ICM/Model/Department.cs
+++ ICM/Model/Department.cs
@@ -1,53 +1,84 @@
 ﻿namespace ICM.Model
 {
+    /// <summary>
+    /// Class department, represents a department belonging to an institution.
+    /// </summary>
     public class Department
     {
+        /// <summary>
+        /// Gives access to the department's id.
+        /// </summary>
         public int Id
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the department's name.
+        /// </summary>
         public string Name {
             get; 
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's id to which the department belongs.
+        /// </summary>
         public int InstitutionId
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the insitution's name to which the department belongs.
+        /// </summary>
         public string InstitutionName
         {
             get; 
             set; 
         }
 
+        /// <summary>
+        /// Gives access to the insitution's name followed by the department's name.
+        /// </summary>
         public string departementInstitution
         {
             get { return InstitutionName + ": " + Name; }
         }
 
+        /// <summary>
+        /// Gives access to the institution's city to which the department belongs.
+        /// </summary>
         public string InstitutionCity
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institutin's country to which the department belongs.
+        /// </summary>
         public string InstitutionCountry
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's language to which the department belongs.
+        /// </summary>
         public string InstitutionLanguage
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// String representing the department.
+        /// </summary>
+        /// <returns>The department's name.</returns>
         public override string ToString()
         {
             return Name;
--- ICM/Model/Institution.cs
+++ ICM/Model/Institution.cs
@@ -5,67 +5,114 @@ using System.Web;
 
 namespace ICM.Model
 {
+    /// <summary>
+    /// Class institution, represents an institution in a contract.
+    /// </summary>
+    /// <remarks>Kean Mariotti</remarks>
     public class Institution
     {
+        /// <summary>
+        /// Gives access to the institution's id.
+        /// </summary>
         public int Id
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's name.
+        /// </summary>
         public string Name
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's description.
+        /// </summary>
         public string Description
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's city.
+        /// </summary>
         public string City
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's interest.
+        /// </summary>
         public string Interest
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's Language.
+        /// </summary>
         public Language Language
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's country.
+        /// </summary>
         public Country Country
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's departments.
+        /// </summary>
         public List<Department> Departments
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gives access to the institution's state (archived or not archived).
+        /// </summary>
         public bool IsArchived
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// String representing the institution.
+        /// </summary>
+        /// <returns>The institution's name</returns>
         public override string ToString()
         {
             return Name;
         }
 
+        /// <summary>
+        /// Institution constructor.
+        /// </summary>
+        /// <param name="Id">Institution's id.</param>
+        /// <param name="Name">Institutin's name.</param>
+        /// <param name="Description">Institution's description.</param>
+        /// <param name="City">Institution's city.</param>
+        /// <param name="Interest">Institutin's insterest.</param>
+        /// <param name="Language">Institution's language.</param>
+        /// <param name="Country">Institution's country.</param>
+        /// <param name="Departments">Institution's department.</param>
+        /// <param name="IsArchived">Institution's state (archived or not archived).</param>
         public Institution( int Id,
                             string Name,
                             string Description,
