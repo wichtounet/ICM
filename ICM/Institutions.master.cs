54e0acdc8ad8172d8291bc80574a0255d9ba99c3
baptistewicht@gmail.com <wichtounet>
2011-05-13 08:58:40 +0200

    some comments

--- ICM/Institutions.master.cs
+++ ICM/Institutions.master.cs
@@ -2,11 +2,19 @@
 
 namespace ICM
 {
+    ///<summary>
+    /// Master page for the "Institutions" section of the site. 
+    ///</summary>
     public partial class InstitutionsMaster : System.Web.UI.MasterPage
     {
+        /// <summary>
+        /// Load the master page
+        /// </summary>
+        /// <param name="sender">The sender of the events</param>
+        /// <param name="e">The args of the event</param>
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //Nothing to do
         }
     }
 }
--- ICM/ShowInstitution.aspx.cs
+++ ICM/ShowInstitution.aspx.cs
@@ -5,16 +5,24 @@ using ICM.Utils;
 
 namespace ICM
 {
+    ///<summary>
+    /// Enable the user to visualize an institution. 
+    ///</summary>
     public partial class ShowInstitution : System.Web.UI.Page
     {
+        /// <summary>
+        /// Fill the page with the informations of the institution. 
+        /// </summary>
+        /// <param name="sender">The sender of the events</param>
+        /// <param name="e">The args of the event</param>
         protected void Page_Load(object sender, EventArgs e)
         {
             Extensions.SqlOperation operation = () =>
             {
                 if (Request.QueryString["institution"] != null)
                 {
-                    int institutionId = Request.QueryString["institution"].ToInt();
-                    Institution institution = new InstitutionsDAO().GetInstitution(institutionId);
+                    var institutionId = Request.QueryString["institution"].ToInt();
+                    var institution = new InstitutionsDAO().GetInstitution(institutionId);
 
                     NameLabel.Text = institution.Name;
                     DescriptionLabel.Text = institution.Description;
@@ -52,17 +60,19 @@ namespace ICM
         {
             Extensions.SqlOperation operation = () =>
             {
-                int institutionId = Request.QueryString["institution"].ToInt();
+                var institutionId = Request.QueryString["institution"].ToInt();
                 new InstitutionsDAO().ArchiveInstitution(institutionId);
                 StateLabel.Text = "Oui";
                 ArchiveButton.Enabled = false;
             };
+
             this.Verified(operation, ErrorLabel);
         }
 
         protected void EditButton_Click(object sender, EventArgs e)
         {
-            int institutionId = Request.QueryString["institution"].ToInt();
+            var institutionId = Request.QueryString["institution"].ToInt();
+
             Response.Redirect("AddInstitution.aspx?institution=" + institutionId);
         }
     }
